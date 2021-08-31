280563a602aaba62b2633eb65d18b4a850f089fe
ChippyChoppy <rebeccamargaret13@gmail.com>
2021-08-31 19:18:41 -0400

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,27 @@
 puts "deleting all"
 Customer.destroy_all
 
+puts "seeding"
+12.times do 
+    Customer.create!(
+        name: Faker::Name.name,
+        email: Faker::Internet.email,
+        active: Faker::Boolean.boolean
+    )
+end
+50.times do
+    Order.create!(
+        customer_id: Customer.all.sample.id,
+        order_date: Faker::Date.between(from: 364.days.ago, to: Date.today),
+        total: Faker::Number.decimal(l_digits: 3, r_digits: 2)
+    )
+end
+80.times do
+    OrderItem.create!(
+        order_id: Order.all.sample.id,
+        item_id: Faker::Number.within(range: 101..500),
+        quantity: Faker::Number.within(range: 1..10),
+        price: Faker::Number.decimal(l_digits: 2,r_digits: 2),
+        tax: 1.25
+    )
+end
