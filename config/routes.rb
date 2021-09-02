ec93ec5038324ea6f27b3ec0373fab9600fda566
ChippyChoppy <rebeccamargaret13@gmail.com>
2021-09-01 23:23:43 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do 
-      resources :customers, only: [:show]
+      resources :customers, only: [:index, :show, :update, :edit]
+      resources :orders, only: [:index, :show]
+      resources :order_items, only: [:index, :show]
     end
   end
 end
